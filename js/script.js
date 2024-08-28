a606925c273dd1353c5db550a38d819b155bd115
aryan25798 <aryan25798@gmail.com>
2024-08-28 19:30:00 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Placeholder for future JavaScript functionality
+// For example, you could implement chatbot functionality here
+
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('This feature is coming soon!');
+    });
+});
